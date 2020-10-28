ef7fbf2c127b319f8b0c76f87486ceae1962fd4d
Alexander Gedeon <agedeon@umich.edu>
2020-10-28 18:31:30 -0400

    parallelism for conversions

--- perfect_seed_finder.py
+++ perfect_seed_finder.py
@@ -29,10 +29,8 @@ def filter_quadseeds(quadfile, outfile):
     quads.sort()
 
     # Internal function to search for seeds
-    def search_seeds(queue, seedlist):
+    def search_seeds(seedlist):
         for line in seedlist:
-            if not queue.empty():
-                break
             line = int(line.strip())
             os.system(f'./find_compactbiomes {int(line)} {int(line) + 1} >> {outfile}')
 
@@ -42,10 +40,9 @@ def filter_quadseeds(quadfile, outfile):
     even_split = len(quads) // num_processes
 
     processpool = []
-    queue = mp.Queue()
     # Create a process for each split, split into even chunks
     for idx in range(num_processes):
-        a = mp.Process(target=search_seeds, args=[queue, quads[idx*even_split:idx*even_split + even_split]])
+        a = mp.Process(target=search_seeds, args=[quads[idx*even_split:idx*even_split + even_split]])
         a.start()
         processpool.append(a)
 
@@ -56,25 +53,48 @@ def filter_quadseeds(quadfile, outfile):
     print(f"Searched {even_split * num_processes} seeds!")
     return outfile
 
-def convert_ppm_to_png(seed, folder, xsize=512, ysize=256):
+def convert_all_ppm_to_png(seedlist, folder, xsize=512, ysize=256):
     """Create png image given seed and folder."""
 
     # Create folder, make sure it exists
     folder = folder.strip().rstrip('/') + "/"
     if not os.path.exists(os.path.dirname(folder)):
         os.makedirs(os.path.dirname(folder))
-    ppm_filepath = folder + str(seed)
 
-    # Call c binary to generate ppm
-    os.system(f'./image_generator {seed} {folder}/ {xsize} {ysize}')
-    time.sleep(0.25)
+    def convert_single_ppm(seed):
+        ppm_filepath = folder + str(seed)
+        # Call c binary to generate ppm
+        os.system(f'./image_generator {seed} {folder}/ {xsize} {ysize}')
+        while not os.path.exists(ppm_filepath + '.ppm'):
+            time.sleep(0.1)
 
-    # Convert ppm to png, remove ppm to save space
-    im = Image.open(f'{ppm_filepath}.ppm')
-    draw = ImageDraw.Draw(im)
-    draw.rectangle([im.width//2 - 20, im.height//2 - 20, im.width//2 + 20, im.height//2 + 20], width=4, outline="#ff0000")
-    im.save(f'{ppm_filepath}.png')
-    os.remove(f'{ppm_filepath}.ppm')
+        # Convert ppm to png, remove ppm to save space
+        im = Image.open(f'{ppm_filepath}.ppm')
+        draw = ImageDraw.Draw(im)
+        draw.rectangle([im.width//2 - 20, im.height//2 - 20, im.width//2 + 20, im.height//2 + 20], width=4, outline="#ff0000")
+        im.save(f'{ppm_filepath}.png')
+        os.remove(f'{ppm_filepath}.ppm')
+    
+    def convert_batch_ppm(miniseedlist):
+        for seed in miniseedlist:
+            seed = int(seed.strip())
+            convert_single_ppm(seed)
+    
+    # Determine number of processes and splits per process
+    num_processes = mp.cpu_count()
+    even_split = len(seedlist) // num_processes
+
+    processpool = []
+
+    # Create a process for each split, split into even chunks
+    for idx in range(num_processes):
+        a = mp.Process(target=convert_batch_ppm, args=[seedlist[idx*even_split:idx*even_split + even_split]])
+        a.start()
+        processpool.append(a)
+
+    # Attempt to join processes
+    for process in processpool:
+        process.join()
 
 @click.command()
 @click.argument('qx', type=int, required=True)
@@ -134,8 +154,7 @@ def main(qx, qy, search_time, force):
     with open(filtered_file) as flfile:
         lines = flfile.readlines()
         print(f"\nConverting {len(lines)} filtered seeds to {img_path}")
-        for line in lines:
-            convert_ppm_to_png(line.strip(), img_path)
+        convert_all_ppm_to_png(lines, img_path)
     
     print("Done!")
 
