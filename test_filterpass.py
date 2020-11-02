599491217824ad67ae16e8babc51099dd55dbf88
Alexander Gedeon <agedeon@umich.edu>
2020-11-02 17:24:38 -0500

    got file splitting donw

--- test_filterpass.py
+++ test_filterpass.py
@@ -37,4 +37,4 @@ idict = ut.get_lookup_table()
 enum_ints = sorted([idict[key] for key in filter])
 enum_ints = [str(val) for val in enum_ints]
 
-os.system(f'./find_filtered_biomes {cpu_count()} 1024 extra/10k_8x0y.txt {" ".join(enum_ints)}')
+os.system(f'./find_filtered_biomes {cpu_count()} 1024 {MASTER_FILE} {" ".join(enum_ints)}')
